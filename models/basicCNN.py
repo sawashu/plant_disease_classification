028c041a66701f384a0df3e8669a40360113adac
Aahan <taahan0810@gmail.com>
2023-02-21 16:05:21 -0600

    corrected the path for the data

--- models/basicCNN.py
+++ models/basicCNN.py
@@ -24,7 +24,8 @@ batch_size = 4
 
 
 # print(os.path.dirname(os.path.abspath(__file__))) 
-data_path = os.path.dirname(os.path.abspath(__file__)) + '/plantifydr_dataset/color'
+data_path = os.getcwd() + os.sep + os.pardir + '/data/raw/plantifydr_dataset/color'
+print(data_path)
 image_paths = []
 classes = []
 
