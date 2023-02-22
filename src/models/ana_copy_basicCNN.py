18588bcdc3f5692d8787728a1eb9a121229b8cd1
ana-e-uk <114711828+ana-e-uk@users.noreply.github.com>
2023-02-21 20:45:24 -0600

    ran my own copy of cnn

--- src/models/ana_copy_basicCNN.py
+++ src/models/ana_copy_basicCNN.py
@@ -4,8 +4,14 @@ import torch
 from torchvision import datasets, transforms
 from torch.utils.data import random_split
 
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
 import matplotlib.pyplot as plt
 import numpy as np
+
+
 
 
 # specify data directory
@@ -14,14 +20,16 @@ batch_size = 32
 
 # define how to transform data and convert to PyTorch tensors
 # training data can be resized, flipped, etc.
-train_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+train_transform = transforms.Compose([transforms.Resize((256, 256)),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 # validation data should not be changed other than Normalized
 validation_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 
 # load image data with datasets.ImageFolder
-dataset = datasets.ImageFolder(data_dir, transform = transform)
+dataset = datasets.ImageFolder(data_dir, transform = train_transform)
 
 # split data into train and validation data
 train_data, validation_data = random_split(dataset, [0.8, 0.2])
@@ -30,9 +38,13 @@ train_data, validation_data = random_split(dataset, [0.8, 0.2])
 
 # pass data to DataLoader
 # generates batches of images and corresponding labels
-dataloader = torch.utils.data.DataLoader(dataset, 
+train_loader = torch.utils.data.DataLoader(train_data, 
                                          batch_size = batch_size, 
                                          shuffle = True)
+
+valid_loader = torch.utils.data.DataLoader(validation_data,
+                                                batch_size = batch_size,
+                                                shuffle = True)
 
 
 # # loop through dataloader to get single batches
@@ -64,5 +76,57 @@ def imshow(image, ax=None, title=None, normalize=True):
 
     return ax
 
-images, labels = next(iter(dataloader))
-print(imshow(images[0], normalize=False))
+# calling imshow
+# images, labels = next(iter(train_loader))
+# imshow(images[0], normalize=False);
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(59536, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 38)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x,1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+if __name__ == '__main__':
+
+    net = Net()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(10):
+
+        running_loss = 0.0
+        for i,data in enumerate(train_loader,0):
+
+            inputs,labels = data
+
+            optimizer.zero_grad()
+
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            if i % 100 == 99:
+                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 100))
+                running_loss = 0.0
+
+    print(f"Finished Training")
+
+    PATH = './basic_cnn_pth'
+    torch.save(net.state_dict(), PATH)
